8f540f98a47f8504f55f9cae9312aa7e13da6586
DarshilChauhan1 <chauhandarshil716@gmail.com>
2023-12-22 11:38:31 +0530

    adding files

---
+++ src/context/TodoContext.js
@@ -0,0 +1,23 @@
+import {useContext, createContext} from 'react';
+
+export const TodoContext = createContext({
+    todos :[
+        {
+            id : 1,
+            todo : "TodoMsg",
+            completed : false
+        }
+    ],
+    addTodo : (todo)=>{},
+    updateTodo : (todo,id)=>{},
+    deleteTodo :(id)=>{},
+    toggleComplete :(id)=>{}
+
+});
+
+
+export const TodoContextProvider = TodoContext.Provider;
+
+export const useTodo = ()=>{
+    return useContext(TodoContext);
+}
